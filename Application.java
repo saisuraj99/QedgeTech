3c5a92e51fe6e580228bc83b8e02fa124870f4f9
saisuraj99 <surajghadiyaram@gmail.com>
2022-03-02 19:10:40 +0530

    feature300 changes has been done by developer mahender

--- Application.java
+++ Application.java
@@ -40,6 +40,14 @@ System.out.print("Enter the limit: ");
 //reads the limit from the user  
 num=sc.nextInt();  
 System.out.println("Armstrong Number up to "+ num + " are: ");  
+
+#feature300 changes done by mahender
+
+public static void main(String args[])     
+{     
+int num;   
+Scanner sc= new Scanner(System.in);  
+System.out.print("Enter the limit: ");  
 for(int i=0; i<=num; i++)  
 //function calling  
 if(isArmstrong(i))  
